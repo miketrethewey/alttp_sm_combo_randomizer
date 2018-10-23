894bce6fcdd679c097d4d60a6994a77c01becf4c
Lenophis <lenophis@gmail.com>
2018-10-23 02:00:55 -0500

    Define logic expectations
    Clarifying Glitches/NoGlitches in LttP
    Adding in tech requirements for all Super Metroid levels
    Outlying banned tech in Super Metroid

--- config/logic.php
+++ config/logic.php
@@ -27,6 +27,7 @@ return [
 	],
 ];
 /*
+-- Link to the Past Minor Glitches --
 canSuperBunny
 canBecomeDWLink
 canSuperSpeed
@@ -34,15 +35,18 @@ canMirrorClip
 canWaterwalk
 canBunnySurf
 canBunnyRevive
+canBombJump
+canDungeonRevive
+
+-- Link to the Past Overworld Glitches --
 canOWYBA
 canMirrorWrap
 canFakeFlute
 canOneFrameClipOW
-canDungeonRevive
-canBombJump
 canBombClip
 canOneFrameClipUW
 
+-- Link to the Past Major Glitches --
 canYBA
 canSpinYBA
 canMirrorDoor
@@ -53,4 +57,41 @@ canAutoSomaria
 canQuadrantGlitch
 canSpinSomaria
 canAutoYBA
+
+
+TODO:
+place IBJ
+
+-- Super Metroid "Lowest" Tech Requirements --
+canWallJump
+canShineSpark
+
+-- Super Metroid "Middle" Tech Requirements --
+canHellRunNorfair
+canSuitlessMaridia
+canCrystalFlash
+canShortCharge
+canMockball
+canMidAirMorph
+canGGG
+canGravityJump
+canBotwoonIceClip
+canDamageBoost - might need more definers for areas that may need difficult damage boosts
+
+-- Super Metroid "Tournament" Tech Requirements --
+canSpringBallJumpMidAir
+canGrappleJump
+canHJLessLavaDive
+canHorizontalBombJump
+canSnailClip
+canBrinstarReserveHoleInOne
+canCWJ - may move down depending on where stuff lands
+canSpongeBath - access to spikey room of death
+canDamageBoostXRayScopeAccess - getting to X-Ray using D-boosts
+
+-- Super Metroid Banned Tech -- 
+canXRayClimbOOB
+canSpaceTime
+canGTCode
+
 */
